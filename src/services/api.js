e51422437dc17818ec4abb907355da1594dd9a31
MrChandru345 <chandru90925@gmail.com>
2026-02-25 00:23:02 +0530

    modified gitigore

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = '4cc8fcb07596d41436fc0026a4fe0258';
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const BASE_URL = 'https://api.themoviedb.org/3';
 const IMG_BASE_URL = 'https://image.tmdb.org/t/p/';
 
